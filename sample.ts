51f61daf7be29b6da10e5ddf50dcf8e2cb770190
Jamil Abushandi <j_daotra@yahoo.com>
2026-03-02 17:47:49 -0500

    first commit

---
+++ sample.ts
@@ -0,0 +1,7 @@
+let str;
+
+str = "Hello, World!";
+
+str = 42;
+
+str = true;
---
+++ steps/payment-plans/MakePayment.ts
@@ -0,0 +1,5 @@
+import { Given, When, Then, DataTable } from '@cucumber/cucumber';
+
+Given(`Start to type your Given step here`, async () => {
+    console.log("This is THe Given step");
+});
